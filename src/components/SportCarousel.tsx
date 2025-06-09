742b034b0aae39ed9fd2e111d4b56dd6b5dcc299
anageno21 <p.makedon@anageno.com>
2025-06-09 19:29:00 +0300

    Fix SportCarousel with dynamic imports for swipe navigation

--- src/components/SportCarousel.tsx
+++ src/components/SportCarousel.tsx
@@ -2,12 +2,14 @@
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import Padel from '../app/padel/page';
-import Football from '../app/football/page';
-import Volleyball from '../app/volleyball/page';
-import Basketball from '../app/basketball/page';
-import Tennis from '../app/tennis/page';
-import Pickleball from '../app/pickleball/page';
+import dynamic from 'next/dynamic';
+
+const Padel = dynamic(() => import('../app/padel/page'), { ssr: false });
+const Football = dynamic(() => import('../app/football/page'), { ssr: false });
+const Volleyball = dynamic(() => import('../app/volleyball/page'), { ssr: false });
+const Basketball = dynamic(() => import('../app/basketball/page'), { ssr: false });
+const Tennis = dynamic(() => import('../app/tennis/page'), { ssr: false });
+const Pickleball = dynamic(() => import('../app/pickleball/page'), { ssr: false });
 
 const SportCarousel = () => {
   const sports = [
@@ -20,11 +22,11 @@ const SportCarousel = () => {
   ];
 
   return (
-    <div className="sm:hidden w-full">
+    <div className="sm:hidden w-full h-full">
       <Swiper
         spaceBetween={0}
         slidesPerView={1}
-        initialSlide={sports.findIndex((sport) => sport.path === window.location.pathname)}
+        initialSlide={sports.findIndex((sport) => sport.path === window.location.pathname) || 0}
         className="w-full h-full"
       >
         {sports.map((sport, index) => (
